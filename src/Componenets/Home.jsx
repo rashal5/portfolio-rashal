95b9d254dd8f283c1af341553d52df471e119ad5
Muhammed Rashal M <136580573+rashal5@users.noreply.github.com>
2024-10-20 21:41:52 +0530

    Update Home.jsx
    chnaged

--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -73,8 +73,8 @@ export const Home = () => {
           },
         })
         .to(img.current, { transform: "translateZ(2200px)", opacity: 0.4 }, 0) 
-        .to(text1.current, { y: -800 }, 0.05, "<")
-        .to(text3.current, { y: -800 }, 0.05, "<") 
+        .to(text1.current, { y: -800 }, 0.045, "<")
+        .to(text3.current, { y: -800 }, 0.06, "<") 
         .to(text2.current, { y: 340 ,opacity:0}, 0.04, "<")
         .fromTo(container.current, 
           { yPercent: 100, scaleY: 1 }, 
