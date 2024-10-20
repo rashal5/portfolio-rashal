c0e0871d6e81631a83ff7d15aa06940777d64e2d
Muhammed Rashal M <136580573+rashal5@users.noreply.github.com>
2024-10-20 21:46:36 +0530

    Update Experience.jsx
    change direction

--- src/Componenets/Experience.jsx
+++ src/Componenets/Experience.jsx
@@ -4,7 +4,7 @@ const Experience = () => {
   return (
     <div className="pb-4 px-8">
       <motion.h2  whileInView={{ opacity:1,y:0}} 
-                initial={{ opacity:0,y:-100}}
+                initial={{ opacity:0,x:-100}}
                 transition={{ duration:0.5}} className="my-20 text-center text-4xl text-[#F3F3F3]">Experience</motion.h2>
       <div>
        
@@ -17,7 +17,7 @@ const Experience = () => {
               </p>
              </motion.div>
             <motion.div whileInView={{ opacity:1,x:0}} 
-                initial={{ opacity:0,x:-100}}
+                initial={{ opacity:0,y:-100}}
                 transition={{ duration:1}} className="w-full max-w-xl lg:w-3/4">
               <h3 className="mb-2 font-semibold text-[#F3F3F3]">
               Software Developer -{" "}
