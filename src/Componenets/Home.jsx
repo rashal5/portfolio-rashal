c193558a68eee9607de34d94cb5071b2c7477a08
Muhammed Rashal M <136580573+rashal5@users.noreply.github.com>
2024-10-22 12:27:11 +0530

    Update Home.jsx

--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -7,7 +7,7 @@ import profilePic from "../assets/img3.png";
 import profilePic1 from "../assets/img2.png";
 import { HERO_CONTENT } from "../constants";
 import { motion } from "framer-motion";
-import {pdf} from "../public/Muhammed Rashal M.pdf";
+import { pdf } from "../public/Muhammed Rashal M.pdf";
 
 
 const containerVariants = {
