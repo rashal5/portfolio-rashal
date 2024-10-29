d2d6c843a8399017c38a291d309314523068f3b5
Muhammed Rashal M <136580573+rashal5@users.noreply.github.com>
2024-10-29 22:14:58 +0530

    Update project.jsx

--- src/Componenets/project.jsx
+++ src/Componenets/project.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <div className="pb-4 px-8">
       <motion.h2 whileInView={{ opacity:1,y:0}} 
                 initial={{ opacity:0,y:-100}}
-                transition={{ duration:0.5}} className="my-20 text-center text-4xl text-[#bcd2e8]">Projects</motion.h2>
+                transition={{ duration:0.5}} className="my-20 text-center text-4xl f5">Projects</motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
@@ -24,8 +24,8 @@ const Projects = () => {
             <motion.div whileInView={{ opacity:1,x:0}} 
                 initial={{ opacity:0,x:100}}
                 transition={{ duration:1}} className="w-full max-w-xl lg:w-3/4">
-              <h3 className="mb-2 font-semibold text-2xl text-[#bcd2e8]">{project.title}</h3>
-              <p className="mb-4 text-stone-400">{project.description}</p>
+              <h3 className="mb-2 font-semibold text-2xl f5">{project.title}</h3>
+              <p className="mb-4 p6">{project.description}</p>
               <a href={project.link} className="ml-2 inline-block rounded bg-blue-600 p-1 text-white">
     GitHub 
   </a>
