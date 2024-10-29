0430bcb1684eb8e059ae71bd3e937b79b0082119
Muhammed Rashal M <136580573+rashal5@users.noreply.github.com>
2024-10-29 21:59:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from './Componenets/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ThemeProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ThemeProvider>
 );
