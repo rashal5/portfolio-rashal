1ebe09ef974ba855a5cc159f8821c30142f305fa
Muhammed Rashal M <136580573+rashal5@users.noreply.github.com>
2024-10-29 22:02:09 +0530

    Update About.jsx

--- src/Componenets/About.jsx
+++ src/Componenets/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="pb-4 px-8">
       <motion.h2  whileInView={{ opacity:1,y:0}} 
                 initial={{ opacity:0,y:-100}}
-                transition={{ duration:0.5}} className="my-20 text-center text-4xl text-[#F3F3F3]">About Me </motion.h2>
+                transition={{ duration:0.5}} className="my-20 text-center text-4xl f5">About Me </motion.h2>
       <div>
        
           <div className="mb-8 flex flex-wrap lg:justify-center">
@@ -15,7 +15,7 @@ const About = () => {
             <motion.div whileInView={{ opacity:1,x:0}} 
                 initial={{ opacity:0,x:-100}}
                 transition={{ duration:1}} className="w-full max-w-xl lg:w-3/4">
-              <p className="mb-4 text-stone-400">
+              <p className="mb-4 p6">
              {ABOUT_TEXT}
               </p>
               
