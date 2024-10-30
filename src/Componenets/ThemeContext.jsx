a5cf4104d75201c85278a243a9982b58e9610fd6
Muhammed Rashal M <136580573+rashal5@users.noreply.github.com>
2024-10-30 11:11:25 +0530

    Update ThemeContext.jsx

--- src/Componenets/ThemeContext.jsx
+++ src/Componenets/ThemeContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
     document.body.classList.remove("light-theme", "dark-theme");
